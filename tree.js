f655df3be957a4be5773a8ee1b03cd60e1f8bb01
sigh <sigh.projects@gmail.com>
2021-09-11 18:17:07 +1000

    Infinite zoom with consistent location.

--- tree.js
+++ tree.js
@@ -3,35 +3,58 @@ let canvas = document.getElementById('tree-vis');
 const DIM = 64;
 const SIZE = 400;
 
-const drawNode = (ctx, x, y, scale, text) => {
-  let size = Math.floor(DIM*scale);
-  ctx.font = `${size}px sans`;
-  ctx.textAlign = 'center';
-  ctx.fillText(text, x, y);
-};
+class Renderer {
+  constructor(canvas, ctx, scale, origin) {
+    this._canvas = canvas;
+    this._ctx = ctx;
+    this._scale = scale;
+    this._origin = origin;
+  }
 
-const drawTree = (ctx) => {
+  clear() {
+  }
+
+  drawNode(x, y, d, text) {
+    const height = this._canvas.height;
+    const width = this._canvas.width;
+
+    const textScale = Math.pow(2, -d) * this._scale;
+    const fontSize = Math.floor(textScale/4 * height);
+
+    x = this._canvas.width * this._scale * (x - this._origin.x);
+    y = this._canvas.height * this._scale * (1-(y-this._origin.y));
+
+    // TODO: Get rid of this hacky optimization.
+    if (x >= -width && y >= -height && x < width * 2 && y < height * 2) {
+      this._ctx.font = `${fontSize}px sans`;
+      this._ctx.textAlign = 'center';
+      this._ctx.fillText(text, x, y);
+    }
+
+    return fontSize;
+  }
+}
+
+const drawTree = (ctx, origin, scale) => {
+  let renderer = new Renderer(canvas, ctx, scale, origin);
   let drawTreeRec = (x, y, d, a, b) => {
     let c = [a[0] + b[0], a[1] + b[1]];
 
-    let scale = Math.pow(2, -d);
+    let edgeScale = Math.pow(2, -d) * 0.25;
 
-    drawNode(ctx, x, y, scale, `${c[0]}/${c[1]}`);
+    let fontSize = renderer.drawNode(x, y, d, `${c[0]}/${c[1]}`);
 
-    if (scale*60 > 1) {
-      drawTreeRec(x-scale*DIM, y+scale*DIM, d+1, a, c);
-      drawTreeRec(x+scale*DIM, y+scale*DIM, d+1, b, c);
+    if (fontSize > 0) {
+      drawTreeRec(x-edgeScale, y-edgeScale, d+1, a, c);
+      drawTreeRec(x+edgeScale, y-edgeScale, d+1, b, c);
     }
   };
-  drawTreeRec(SIZE/2, DIM, 0, [0,1], [1,0]);
+  drawTreeRec(0.5, 0.75, 0, [0,1], [1,0]);
 };
-drawTree(canvas.getContext('2d'), 1);
+drawTree(canvas.getContext('2d'), {x:0, y:0}, 1);
 
 const clearCanvas = (ctx) => {
-  ctx.save();
-  ctx.setTransform(1, 0, 0, 1, 0, 0);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.restore();
 };
 
 const deferUntilAnimationFrame = (fn) => {
@@ -61,16 +84,16 @@ const deferUntilAnimationFrame = (fn) => {
   });
 };
 
-const redraw = deferUntilAnimationFrame(() => {
+const redraw = deferUntilAnimationFrame((scale, origin) => {
   let ctx = canvas.getContext('2d');
   clearCanvas(ctx);
-  drawTree(ctx);
+  drawTree(ctx, scale, origin);
 });
 
 class Viewport {
   constructor(canvas) {
     this._logScale = 0;
-    this._center = {x: 0.5, y: 0.5};
+    this._origin = {x: 0, y: 0};
 
     this._canvas = canvas;
     this._ctx = canvas.getContext('2d');
@@ -82,40 +105,45 @@ class Viewport {
     canvas.onwheel = (e) => {
       e.preventDefault();
 
-      const pixelScale = this._pixelScale();
-      const canvasCenter = this._canvasCenter();
+      const canvasOrigin = this._canvasOrigin();
+      const canvasX = e.clientX - canvasOrigin.x;
+      const canvasY = e.clientY - canvasOrigin.y;
 
-      const dx = pixelScale * (e.clientX - canvasCenter.x);
-      const dy = pixelScale * (e.clientY - canvasCenter.y);
+      let pixelScale = this._pixelScale();
+      let dx = pixelScale * canvasX;
+      let dy = pixelScale * canvasY;
 
       let ds = e.deltaY * 0.01;
       ds = ds > 0.5 ? 0.5 :
            ds < -0.5 ? -0.5 :
            ds;
+      ds = Math.max(ds, -this._logScale);
 
-      this._logScale = Math.max(0, this._logScale + ds);
+      this._logScale += ds;
 
-      this._center.x -= dx*ds;
-      this._center.y += dy*ds;
+      pixelScale = this._pixelScale();
+      dx -= pixelScale * canvasX;
+      dy -= pixelScale * canvasY;
+
+      // TODO: Figure out a simpler expression for dx and dy.
+      this._origin.x += dx;
+      this._origin.y -= dy;
 
       this._update();
     };
   }
 
   _pixelScale() {
-    return Math.pow(2, this._logScale) / (this._canvas.clientHeight);
+    return Math.pow(2, -this._logScale) / (this._canvas.clientHeight);
   }
 
-  _canvasCenter() {
+  _canvasOrigin() {
     const bb = this._canvas.getBoundingClientRect();
-    return {x: bb.x + bb.width/2, y: bb.y + bb.height/2};
+    return {x: bb.x, y: bb.y};
   }
 
   _update() {
-    this._ctx.setTransform(1, 0, 0, 1, 0, 0);
-    let scale = Math.pow(2, this._logScale);
-    this._ctx.scale(scale,scale);
-    redraw();
+    redraw(this._origin, Math.pow(2, this._logScale));
   }
 }
 
